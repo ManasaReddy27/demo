aa107e78627878f424f3b24046acf7b27342e1c4
Sudeep <Sudeep@Sudeep.home>
2017-04-20 20:29:42 -0400

    Adding new classes

---
+++ src/duplicatenumber.java
@@ -0,0 +1,16 @@
+
+public class duplicatenumber {
+
+	public static void main(String[] args) {
+		int[] numbers = { 1, 5, 23, 2, 1, 6, 3, 1, 8, 12, 3 };
+		
+
+		for(int i = 1; i < numbers.length; i++) {
+		    if(numbers[i] == numbers[i - 1]) {
+		        System.out.println("Duplicate: " + numbers[i]);
+		    }
+		}
+
+	}
+
+}
